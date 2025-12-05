3f8c165c522ae24b4cedff2034b3530a82ee4c10
jorcascan <jorgecases2000@gmail.com>
2025-12-05 13:16:23 +0100

    Nombre cambiado

--- HolaGitIntelliJ.java
+++ HolaGitIntelliJ.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("¡Hola desde IntelliJ y Git!");
-        mostrarSaludo("Pablo");
+        mostrarSaludo("Jorge");
     }
     public static void mostrarSaludo(String nombre) {
         System.out.println("¡Hola, " + nombre + "! Bienvenido al proyecto.");
